935d0bea8229f000878bb7803f0f3addb7c23b98
ilanMa <ilan.mamontov@gmail.com>
2016-04-09 12:23:36 -0400

    Work on initializing Youtube App

---
+++ ReduxStart/src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// Same as: cost Component = React.Component'
+
+class SearchBar extends Component {
+	render() {
+		return <input />
+	}
+}
+
+export default SearchBar;
