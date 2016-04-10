04bd497505fa0dc51a957b3366115e9599a3397e
ilanMa <ilan.mamontov@gmail.com>
2016-04-10 00:29:15 -0400

    Work on input field

--- ReduxStart/src/components/search_bar.js
+++ ReduxStart/src/components/search_bar.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 // Same as: cost Component = React.Component'
 
 class SearchBar extends Component {
+		constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
 	render() {
-		return <input />
+		return (
+			<input value={this.state.term} onChange={event => this.setState({term: event.target.value})} />
+		)
 	}
 }
 
