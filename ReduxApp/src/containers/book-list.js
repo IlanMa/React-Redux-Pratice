7f21da95b8538e798fa7a73eb199b745dfdd9d05
ilanMa <ilan.mamontov@gmail.com>
2016-04-16 12:23:44 -0400

    Implement book container

---
+++ ReduxApp/src/containers/book-list.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+	renderList() {
+		return this.props.books.map((books) => {
+			return (
+				<li key={books.title} className="list-group-item"> {books.title} </li>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<ul className="list-group-col-sm-4">
+				{this.renderList()}
+			</ul>
+		)
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		books: state.books
+	};
+}
+
+export default connect(mapStateToProps)(BookList);
