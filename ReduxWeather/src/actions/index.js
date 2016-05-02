120e791b6feac43494a6eecf0af0aa673295e407
ilanMa <ilan.mamontov@gmail.com>
2016-05-01 20:56:44 -0400

    Add redux-promise and axios

---
+++ ReduxWeather/src/actions/index.js
@@ -0,0 +1,15 @@
+const API_KEY = '35bc680a09a825c23cdd44e0f62e43c9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`
+	const request = axios.get(url);
+
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
