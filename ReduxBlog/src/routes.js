8258666651850af6b70823ca9709c1190684f169
ilanMa <ilan.mamontov@gmail.com>
2016-05-17 18:19:59 -0400

    Add reducers and actions

--- ReduxBlog/src/routes.js
+++ ReduxBlog/src/routes.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Router, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/post_index';
 
 export default (
-	<Router path="/" component={App} />
-)
+	<Router path="/" component={App}>
+		<IndexRoute component={PostsIndex} />
+	</Router>
+);
