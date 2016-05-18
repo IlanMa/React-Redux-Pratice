7d5d852658279826b43a6876f3d7bf46bb15a346
ilanMa <ilan.mamontov@gmail.com>
2016-05-18 19:02:55 -0400

    Add post form w/ api & validation

--- ReduxBlog/src/routes.js
+++ ReduxBlog/src/routes.js
@@ -3,9 +3,11 @@ import { Router, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/post_index';
+import PostNew from './components/post_new';
 
 export default (
 	<Router path="/" component={App}>
 		<IndexRoute component={PostsIndex} />
+		<Router path="posts/new" component={PostNew} />
 	</Router>
 );
