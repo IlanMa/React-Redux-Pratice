3930e2b5aa685e0ceb60188a76b75378cd7a2dbd
Ilan <ilan.mamontov@gmail.com>
2017-05-07 17:39:07 -0400

    More testing setup

---
+++ ReactTesting/src/components/comment_box.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'React';
+
+export default class CommentBox extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { commen: '' };
+	}
+
+
+	handleChange(event) {
+		this.setState({ comment: event.target.value });
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		this.setState({ comment: '' })
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+				<textarea 
+					value={this.state.comment}
+					onChange={this.handleChange.bind(this)} />
+				<button action='submit'>Button</button>
+			</form>
+		);
+	}
+}
--- ReactTesting/test/components/app_test.js
+++ ReactTesting/test/components/app_test.js
@@ -6,12 +6,17 @@ import App from '../../src/components/app';
 
 describe('App', () => {
 
-  it('shows the correct text', () => {
-
-  	const component = renderComponent(App);
-
-    expect(component).to.contain('React simple starter');
-
+  let component;
+  beforeEach(() => {
+    component = renderComponent(App);
   })
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment list', () => {
+  	expect(component.find('.comment-list')).to.exist;
+  });
 
 });
