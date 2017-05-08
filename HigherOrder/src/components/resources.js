9e25d5428af7d4c4b3e2221a0c82fa3ee59a4fad
Ilan <ilan.mamontov@gmail.com>
2017-05-08 18:52:25 -0400

    Partial high order components

---
+++ HigherOrder/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Recipe
+			<ul>
+				<li>Ingredient 1</li>
+				<li>Ingredient 2</li>
+				<li>Ingredient 3</li>
+			</ul>
+		</div>
+	)
+}
